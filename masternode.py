e4f5446f487616fdeec8482eea7b52f43d2d52cb
Jobin Jose <jobinjose@outlook.com>
2017-12-09 16:14:44 +0000

    adding master

--- masternode.py
+++ masternode.py
@@ -1,6 +1,7 @@
 import web
 import shelve
 import os
+import git
 import sys
 
 urls = (
@@ -14,20 +15,24 @@ class masterapp(web.application):
         return web.httpserver.runsimple(func, ('0.0.0.0', port))
 
 class mainclass:
-    def GET(self,filename):
-        s = shelve.open('dir_db')
-        try:
-            f_dir = s[filename]
-        finally:
-            s.close()
-        fullpath = os.path.join(f_dir, filename)
-        f = open(fullpath,"r")
-        return f.read()
+    def GET(self):
+        return
+    def POST(self):
+        return
 
 class redirect:
     def GET(self, path):
         web.seeother('/' + path)
 
 if __name__ == "__main__":
+    repo = git.Repo("C:/Users/Jobin/Documents/GitHub/carousel")
+    commits_list = list(repo.iter_commits('master'))
+    i=1
+    for commit in commits_list:
+        for file_key in commit.stats.files.keys():
+            web.config.filelist_per_commit[i] = [commit.hexsha,file_key]
+            i=i+1
+    print(len(web.config.filelist_per_commit))
+
     app = masterapp(urls, globals())
-    app.run(port=8866)
+    app.run(port=8080)
